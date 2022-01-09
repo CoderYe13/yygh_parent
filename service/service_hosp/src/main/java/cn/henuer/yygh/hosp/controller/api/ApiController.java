3a4b577251c5f96178ac0707e2891b59f1a0d03d
aipl <YEY0929@163.com>
2022-01-09 22:57:46 +0800

    完成service_hosp上传医院科室接口和删除接口

--- service/service_hosp/src/main/java/cn/henuer/yygh/hosp/controller/api/ApiController.java
+++ service/service_hosp/src/main/java/cn/henuer/yygh/hosp/controller/api/ApiController.java
@@ -1,14 +1,20 @@
-package cn.henuer.yygh.hosp.api;
+package cn.henuer.yygh.hosp.controller.api;
 
+import cn.henuer.model.hosp.Department;
 import cn.henuer.model.hosp.Hospital;
+import cn.henuer.vo.hosp.DepartmentQueryVo;
 import cn.henuer.yygh.common.exception.HospitalException;
 import cn.henuer.yygh.common.result.Result;
 import cn.henuer.yygh.common.result.ResultCodeEnum;
 import cn.henuer.yygh.hosp.commom.helper.HttpRequestHelper;
 import cn.henuer.yygh.hosp.commom.utils.MD5;
+import cn.henuer.yygh.hosp.service.DepartmentService;
 import cn.henuer.yygh.hosp.service.HospitalService;
 import cn.henuer.yygh.hosp.service.HospitalSetService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,13 +24,83 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api/hosp")
-public class HospitalController {
+public class ApiController {
 
     @Autowired
     private HospitalService hospitalService;
 
     @Autowired
     private HospitalSetService hospitalSetService;
+
+    @Autowired
+    private DepartmentService departmentService;
+
+    //删除科室接口
+    @PostMapping("department/remove")
+    public Result removeDepartment(HttpServletRequest request){
+        //获取到传递过来的信息
+        Map<String, String[]> requestMap = request.getParameterMap();
+        Map<String,Object> paramMap= HttpRequestHelper.switchMap(requestMap);
+
+        //医院编号和科室编号
+        String hoscode= (String) paramMap.get("hoscode");
+        String depcode=(String) paramMap.get("depcode");
+
+        //TODO校验签名
+        departmentService.remove(hoscode,depcode);
+        return Result.ok();
+    }
+
+    //查询科室接口
+    @PostMapping("department/list")
+    public Result findDepartment(HttpServletRequest request){
+        //获取到传递过来的信息
+        Map<String, String[]> requestMap = request.getParameterMap();
+        Map<String,Object> paramMap= HttpRequestHelper.switchMap(requestMap);
+
+        //1、获取医院编号
+        String hoscode = (String)paramMap.get("hoscode");
+        //当前页码和每页的记录数
+        int page =StringUtils.isEmpty((String)paramMap.get("page"))?1:
+               Integer.parseInt((String) paramMap.get("page"));
+        int limit =StringUtils.isEmpty((String)paramMap.get("limit"))?1:
+                Integer.parseInt((String) paramMap.get("limit"));
+        //TODO签名校验
+
+        DepartmentQueryVo departmentQueryVo=new DepartmentQueryVo();
+        departmentQueryVo.setHoscode(hoscode);
+
+        //调用service通过page-1,limit,departmentQueryVo
+       Page<Department> pageModel=departmentService.findPageDepartment(page,limit,departmentQueryVo);
+        return Result.ok(pageModel);
+    }
+
+    //上传科室接口
+    @PostMapping("saveDepartment")
+    public Result saveDepartment(HttpServletRequest request){
+        //获取到传递过来的信息
+        Map<String, String[]> requestMap = request.getParameterMap();
+        Map<String,Object> paramMap= HttpRequestHelper.switchMap(requestMap);
+
+        //1、获取医院系统接口传递过来的签名，签名进行MD5加密
+        String hospSign=(String) paramMap.get("sign");
+        //2、根据传递过来的医院编码，查询数据库，查询签名
+        String hoscode=(String)paramMap.get("hoscode");
+        String signKey=hospitalSetService.getSignKey(hoscode);
+
+        //3、吧数据库查询签名进行MD5加密
+        String signKeyMd5=MD5.encrypt(signKey);
+
+        //4、判断签名是否一致
+        if(!hospSign.equals(signKeyMd5)){
+            throw new HospitalException(ResultCodeEnum.SIGN_ERROR);
+        }
+        //调用service的方法
+        departmentService.save(paramMap);
+
+        return Result.ok();
+    }
+
 
     //上传医院接口
     @PostMapping("saveHospital")
---
+++ service/service_hosp/src/main/java/cn/henuer/yygh/hosp/service/DepartmentService.java
@@ -0,0 +1,17 @@
+package cn.henuer.yygh.hosp.service;
+
+import cn.henuer.model.hosp.Department;
+import cn.henuer.vo.hosp.DepartmentQueryVo;
+import org.springframework.data.domain.Page;
+
+import java.util.Map;
+
+public interface DepartmentService {
+    //上传科室接口
+    void save(Map<String, Object> paramMap);
+
+    //查询科室接口
+    Page<Department> findPageDepartment(int page, int limit, DepartmentQueryVo departmentQueryVo);
+    //删除科室接口
+    void remove(String hoscode, String depcode);
+}
