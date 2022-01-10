6fb5aa16c19292adc7a8cd2a6e0e7610f6afcf8e
aipl <YEY0929@163.com>
2022-01-10 21:53:35 +0800

    完成service服务在nacos中注册

--- service/service_cmn/src/main/java/cn/henuer/yygh/cmn/ServiceCmnApplication.java
+++ service/service_cmn/src/main/java/cn/henuer/yygh/cmn/ServiceCmnApplication.java
@@ -2,10 +2,12 @@ package cn.henuer.yygh.cmn;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "cn.henuer")
+@EnableDiscoveryClient
 public class ServiceCmnApplication {
     public static void main(String[] args) {
         SpringApplication.run(ServiceCmnApplication.class,args);
