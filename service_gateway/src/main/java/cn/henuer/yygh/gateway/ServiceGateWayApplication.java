79ce92fe7e7fe0a0a56a1b56fda1faf48db0b05d
aipl <YEY0929@163.com>
2022-01-15 23:39:59 +0800

    完成service_gateway

---
+++ service_gateway/src/main/java/cn/henuer/yygh/gateway/ServiceGateWayApplication.java
@@ -0,0 +1,11 @@
+package cn.henuer.yygh.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceGateWayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceGateWayApplication.class,args);
+    }
+}
